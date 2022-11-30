39b4ac752b75e0ff9a41019eb4e766ff1fa4304f
cahyosubroto <cahyosubroto30@gmail.com>
2022-11-30 11:52:25 +0700

    edit the base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Linkorb.io',
   tagline: 'Dinosaurs are cool',
   url: 'https://cahyosubroto.github.io',
-  baseUrl: '/Linkorb.io/API/',
+  baseUrl: '/Linkorb.io/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
